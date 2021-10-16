9fa1a8ccdae051ed77a3e663b8fb4f0af10a6b5e
Rohitsarswat <83912817+Rohitsarswat@users.noreply.github.com>
2021-10-16 17:35:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,24 @@
 
-var box1;
+var box1, box2;
 
 function setup() {
   createCanvas(600, 400);
-  
+  box1 = new Box();
+  box2 = new Box();
 }
 
 function draw() {
   background(220);
+
+  box1.x = 300;
+  box1.show();
+  box1.setWidth(100);
+
+  box2.x = 100;
+  box2.show();
+  box2.setWidth(30);
+  
+  
 
 }
 
